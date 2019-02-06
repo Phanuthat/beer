b9c07838e997592a632c2fb239d3cf07f1409f22
Phanuthat <phanuthat.jun@gmail.com>
2019-02-06 18:03:30 +0700

    add function calculate

--- src/containers/CartPage.js
+++ src/containers/CartPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button} from 'antd';
+
 import { connect } from 'react-redux';
 import ListCart from '../components/Cart/list_cart';
 
@@ -12,6 +12,7 @@ const mapStateToProps = state => {
 class CartPage extends Component {
   getItems = () => {
     const jsonStr = localStorage.getItem('user-data');
+    
     const email = jsonStr && JSON.parse(jsonStr).email;
     const jsonCartStr = localStorage.getItem(`beer-ja-list-cart-${email}`);
     if (jsonCartStr) {
@@ -19,6 +20,39 @@ class CartPage extends Component {
       return items;
     }
   };
+
+  calculate = () =>{
+
+      const kuys = this.getItems()
+      console.log("ssss",kuys[0].attenuation_level);
+      let total = 0
+      for(let i of kuys ){
+            total+=i.attenuation_level
+            
+      }
+      return total 
+
+
+
+
+
+
+    // const jsonStr = localStorage.getItem('user-data');
+    // const price = jsonStr && JSON.parse(jsonStr).attenuation_level;
+    // const jsonCartStr = localStorage.getItem(`beer-ja-list-cart-${price}`);
+    // // const jsonCartStr = localStorage.getItem(`beer-ja-list-cart-${price}`);
+    // if (jsonCartStr){
+    //  const Price = jsonCartStr && JSON.parse(jsonCartStr);
+    //   // for(let i=0 ; sumPrice.length>i;i++ ){
+    //   //   let total = 0
+    //   //   return total +=sumPrice[i]
+    //   // }
+    //    return Price ;
+    // }
+
+
+
+  }
 
   // total() {
   //   return this.props.items.reduce((sum, item) => {
@@ -38,9 +72,9 @@ class CartPage extends Component {
 
   render() {
     console.log(this.props.isShowDialog);
-    console.log(this.price);
-    console.log(this.jsonStr);
-    console.log(this.jsonCartStr);
+
+    console.log("buy item" ,this.getItems());
+    console.log("buy item" ,this.calculate());
     return (
       <div
         style={{
@@ -49,8 +83,9 @@ class CartPage extends Component {
           minHeight: '600px'
         }}
       >
-        <ListCart items={this.getItems()} />
-
+      <div>total ={'TOtal' + this.calculate()}</div>
+        {/* <ListCart items={this.getItems()} /> */}
+        <ListCart items={this.getItems()}  />
         <div>
           {/* <Button
             type="primary"
