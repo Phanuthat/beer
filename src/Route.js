75ccd241c561b9d0da28e72e168c1acbdd841279
Phanuthat <phanuthat.jun@gmail.com>
2019-02-06 20:09:50 +0700

    add route checkout

--- src/Route.js
+++ src/Route.js
@@ -3,13 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import LoginPage from './containers/LoginPage';
 import MainPage from './containers/MainPage';
 import RegisterPage from './containers/RegisterPage';
-
+import Checkout from './containers/Checkout'
 function Routes() {
   return (
     <div style={{ width: '100%' }}>
       <Switch>
         <Route exact path="/" component={LoginPage} />
         <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/checkout" component={Checkout} />
         <Route component={MainPage} />
       </Switch>
     </div>
