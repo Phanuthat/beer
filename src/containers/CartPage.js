cc63c2101abb6cc54a297b84b8ee18daafb809a2
Phanuthat <phanuthat.jun@gmail.com>
2019-02-06 19:59:51 +0700

    add function calculate

--- src/containers/CartPage.js
+++ src/containers/CartPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Layout, Menu, Modal, Button, message, Icon } from 'antd';
 import { connect } from 'react-redux';
 import ListCart from '../components/Cart/list_cart';
 
@@ -12,7 +12,7 @@ const mapStateToProps = state => {
 class CartPage extends Component {
   getItems = () => {
     const jsonStr = localStorage.getItem('user-data');
-    
+
     const email = jsonStr && JSON.parse(jsonStr).email;
     const jsonCartStr = localStorage.getItem(`beer-ja-list-cart-${email}`);
     if (jsonCartStr) {
@@ -21,60 +21,25 @@ class CartPage extends Component {
     }
   };
 
-  calculate = () =>{
+  calculate = () => {
+    const kuys = this.getItems();
+    console.log('ssss', kuys[0].attenuation_level);
+    let total = 0;
+    for (let i of kuys) {
+      total += i.attenuation_level;
+    }
+    return total;
 
-      const kuys = this.getItems()
-      console.log("ssss",kuys[0].attenuation_level);
-      let total = 0
-      for(let i of kuys ){
-            total+=i.attenuation_level
-            
-      }
-      return total 
+   
+  };
 
-
-
-
-
-
-    // const jsonStr = localStorage.getItem('user-data');
-    // const price = jsonStr && JSON.parse(jsonStr).attenuation_level;
-    // const jsonCartStr = localStorage.getItem(`beer-ja-list-cart-${price}`);
-    // // const jsonCartStr = localStorage.getItem(`beer-ja-list-cart-${price}`);
-    // if (jsonCartStr){
-    //  const Price = jsonCartStr && JSON.parse(jsonCartStr);
-    //   // for(let i=0 ; sumPrice.length>i;i++ ){
-    //   //   let total = 0
-    //   //   return total +=sumPrice[i]
-    //   // }
-    //    return Price ;
-    // }
-
-
-
-  }
-
-  // total() {
-  //   return this.props.items.reduce((sum, item) => {
-  //     return sum + item.attenuation_level;
-  //   }, 0);
-  // }
-
-  // total = () => {
-  //   const jsonStr = localStorage.getItem('user-data');
-  //   const price = jsonStr && JSON.parse(jsonStr).attenuation_level;
-  //   for (let i = 0; price.length > i; i++) {
-  //     let sum = 0;
-  //     return (sum += price[i]);
-  //   }
-  //   console.log(price);
-  // };
+ 
 
   render() {
     console.log(this.props.isShowDialog);
 
-    console.log("buy item" ,this.getItems());
-    console.log("buy item" ,this.calculate());
+    console.log('buy item', this.getItems());
+    console.log('buy item', this.calculate());
     return (
       <div
         style={{
@@ -83,17 +48,17 @@ class CartPage extends Component {
           minHeight: '600px'
         }}
       >
-      <div>total ={'TOtal' + this.calculate()}</div>
-        {/* <ListCart items={this.getItems()} /> */}
-        <ListCart items={this.getItems()}  />
-        <div>
-          {/* <Button
-            type="primary"
-            // loading={this.state.isLoading}
-            // onClick={this.onClickLogout}
-          >
-            Logout
-          </Button> */}
+        
+        
+        <ListCart items={this.getItems()} />
+        <div class="total">
+          <p>{'Discount 0 $'}</p>
+        </div>
+        <div class="total">
+          <p>{'Total ' + this.calculate() + '$'}</p>
+        </div>
+        <div class = "button-right">
+          <Button type="danger" >Check out</Button>
         </div>
       </div>
     );
