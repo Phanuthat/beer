791fee2eb4bceba4b8b6b5717aa17fbc8093b70c
Phanuthat <phanuthat.jun@gmail.com>
2019-02-06 20:10:24 +0700

    add  checkout

---
+++ src/containers/Checkout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+    render() {
+      return (
+        <div style={{ minHeight: '300px' }}>
+          CommingSoonnnnnnnnnnnnnnnnnn
+        </div>
+      );
+    }
+  }
+  
+  export default Checkout;
+  
