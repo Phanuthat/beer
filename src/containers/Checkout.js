62f2ebaa54e5db2c40a13719c4e367ab73af2e56
Phanuthat <phanuthat.jun@gmail.com>
2019-02-06 20:42:12 +0700

    add commingsoon Checkout page

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import { Spin, Icon } from 'antd';
 class Checkout extends Component {
     render() {
       return (
         <div style={{ minHeight: '300px' }}>
+        <div><Icon type="loading" style={{ fontSize: 50 }} spin /></div>
+        
           CommingSoonnnnnnnnnnnnnnnnnn
         </div>
       );
